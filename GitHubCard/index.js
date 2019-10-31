14d958aaf9a959962bf7903b26532147f8a57ec1
Aaron <aarongillies88@gmail.com>
2019-10-31 18:29:26 -0400

    completed first part of project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,79 @@
+
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+  
+    
 
+      
+
+
+      axios.get("https://api.github.com/users/Hail91")
+      .then(res => {
+        const myCard = gitInfo(res);
+        const newCard = document.querySelector('.cards');
+        newCard.appendChild(myCard);
+      });
+
+      
+
+
+
+
+      //Functional component
+
+    function gitInfo(createCard) {
+      
+      // Creating Elements
+
+      const card = document.createElement('div');
+      const cardData = document.createElement('div');
+      const userImg = document.createElement('img');
+      const userName = document.createElement('h3');
+      const userID = document.createElement('p');
+      const userLoc = document.createElement('p');
+      const userProf = document.createElement('p');
+      const userPage = document.createElement('a');
+      const userFollowers = document.createElement('p');
+      const userFollowing = document.createElement('p');
+      const userBio = document.createElement('p');
+
+      // Add Classes 
+
+      card.classList.add('card');
+      cardData.classList.add('card-info');
+      userName.classList.add('name');
+      userID.classList.add('username');
+
+      // Append elements
+
+      card.appendChild(userImg);
+      card.appendChild(cardData);
+      cardData.appendChild(userName);
+      cardData.appendChild(userID);
+      cardData.appendChild(userLoc);
+      cardData.appendChild(userProf);
+      cardData.appendChild(userPage);
+      cardData.appendChild(userFollowers);
+      cardData.appendChild(userFollowing);
+      cardData.appendChild(userBio);
+
+      // Adding Content 
+
+      userImg.src = createCard.data.avatar_url;
+      userName.textContent = createCard.data.name;
+      userID.textContent = createCard.data.login;
+      userLoc.textContent = createCard.data.location;
+      userPage.textContent = createCard.data.html_url;
+      userFollowers.textContent = `Followers: ${createCard.data.followers}`;
+      userFollowing.textContent = `Following: ${createCard.data.following}`;
+      userBio.textContent = createCard.data.bio;
+
+      return card;
+    }
+         
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -26,6 +97,11 @@
 
 const followersArray = [];
 
+  axios.get("https://api.github.com/users/Hail91")
+  .then(res => {
+      
+  });
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -45,6 +121,8 @@ const followersArray = [];
 </div>
 
 */
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
